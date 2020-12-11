3362cb1cda51b85d2efd70101f6d855a105c5812
anilchoudhary11 <anil.choudhary@innovations.com>
2020-12-11 20:38:07 +0530

    new commit

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -233,8 +233,7 @@
 
 @section('script')
 
-<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
 
 <script src="{{ asset('assets/libs/flatpickr/flatpickr.min.js') }}"></script>
 <script src="{{ asset('assets/libs/select2/select2.min.js') }}"></script>
