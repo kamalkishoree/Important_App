9f0c2142cccb98e0da4ac46d0c9a316c7dc540c4
anilchoudhary11 <anil.choudhary@innovations.com>
2020-12-11 18:09:29 +0530

    fix dashboard issue

--- resources/views/layouts/shared/footer-script.blade.php
+++ resources/views/layouts/shared/footer-script.blade.php
@@ -4,29 +4,14 @@
 
 <script src="{{asset('assets/js/vendor.min.js')}}"></script>
 <script src="{{asset('assets/js/waitMe.min.js')}}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB85kLYYOmuAhBUPd7odVmL6gnQsSGWU-4&libraries=places" async defer></script>
-<div id="my_mapZX" style="display: none;"></div>
 
-<!--<script src="https://maps.googleapis.com/maps/api/js?key={{session('preferences.map_key_1')}}&libraries=places" async defer> </script> -->
+
+
 
 
 <script>
 
-  $(document).ready(function(){
-    checkMap();
-  });
 
-
-
-  function checkMap() {
-
-    var mapProp = {
-        center: new google.maps.LatLng(38, -78),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById('my_mapZX'), mapProp);
-  }
 
 const startLoader = function(element) {
     // check if the element is not specified
@@ -46,10 +31,7 @@ const startLoader = function(element) {
     });
 }
 
-function gm_authFailure() {
-    $('.excetion_keys').append('<span><i class="mdi mdi-block-helper mr-2"></i> <strong>Google Map</strong> key is not valid</span><br/>');
-    $('.displaySettingsError').show();
- };
+
 
 const stopLoader = function(element) {
     // check if the element is not specified
@@ -60,20 +42,6 @@ const stopLoader = function(element) {
     $(element).waitMe("hide");
 }
 
-function initial(window, google, lat, lng) {
-   var options = {
-       center: {
-           lat: Number(lat),
-           lng: Number(lng)
-       },
-       zoom: 5,
-       disableDefaultUI: true,
-       scrollwheel: true,
-       draggable: false
-   },
-   element = document.getElementById('map-canvas'),
-   map = new google.maps.Map(element, options);
-};
 
 
 </script>
