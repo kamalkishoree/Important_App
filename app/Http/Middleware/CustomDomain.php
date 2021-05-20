70cd1ed8e3ee4c60b3d67cd1392903b4b9d94611
dkdenni7 <dkdenni7@gmail.com>
2021-05-20 17:40:57 +0530

    custom domain

--- app/Http/Middleware/CustomDomain.php
+++ app/Http/Middleware/CustomDomain.php
@@ -27,6 +27,7 @@ class CustomDomain
       $domain = $request->getHost();
       // $domain    = str_replace(array('http://', '.dispatcher.test/login'), '', $domain);
       $domain    = str_replace(array('http://', config('domainsetting.domain_set')), '', $domain);
+      $domain    = str_replace(array('https://', config('domainsetting.domain_set')), '', $domain);
       $subDomain = explode('.', $domain);
       //dd($domain); yo.com ,
       
