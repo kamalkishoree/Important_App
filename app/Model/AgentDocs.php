d25fb21a96fa8472a019922d39e950c2e0ee1c14
Manmeet <manmeet.k@codebrewinnovations.com>
2021-09-10 12:30:40 +0530

    fix

--- app/Model/AgentDocs.php
+++ app/Model/AgentDocs.php
@@ -16,4 +16,11 @@ class AgentDocs extends Model
 
     protected $fillable = ['agent_id','file_name','file_type'];
    
+    public function getfileNameAttribute($value){
+        $secret = '';
+        $server = 'http://192.168.100.211:8888';
+        //$new    = \Thumbor\Url\Builder::construct($server, $secret, 'http://images.example.com/llamas.jpg')->fitIn(90,50);
+        return    \Storage::disk("s3")->url($this->file_name);
+        ///return $new; 
+      }
 }
