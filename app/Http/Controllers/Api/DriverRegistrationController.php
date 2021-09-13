ebbc340e9b9131a17192e1a1bee1df9473f67d6b
Manmeet <manmeet.k@codebrewinnovations.com>
2021-09-13 19:05:41 +0530

    fix

--- app/Http/Controllers/Api/DriverRegistrationController.php
+++ app/Http/Controllers/Api/DriverRegistrationController.php
@@ -56,7 +56,7 @@ class DriverRegistrationController extends Controller
             ];
         
         //var_dump(count($request->filedata->all()));
-        $count = count($request->filedata->all());
+        $count = count($_FILES);
         $key = 0;
         $files = [];
         while ($count--) {
