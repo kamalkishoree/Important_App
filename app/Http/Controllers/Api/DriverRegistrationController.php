2eb3359c5b2570c0911afa54643f6440969b55b9
Manmeet <manmeet.k@codebrewinnovations.com>
2021-09-14 14:14:28 +0530

    fix

--- app/Http/Controllers/Api/DriverRegistrationController.php
+++ app/Http/Controllers/Api/DriverRegistrationController.php
@@ -42,6 +42,7 @@ class DriverRegistrationController extends Controller
             $path = Storage::disk('s3')->put($s3filePath, $file, 'public');
             $getFileName = $path;
         }
+
         $data = [
             'name' => $request->name,
             'type' => $request->type,
@@ -71,18 +72,20 @@ class DriverRegistrationController extends Controller
                     $s3filePath = '/assets/' . $folder . '/agents' . $file_name;
                     $path = Storage::disk('s3')->put($s3filePath, $f, 'public');
                     $files[$key] = [
-                        'file_type' => $f->file_type,
-                        'agent_id' => $f->id,
+                        'file_type' => $f['file_type'],
+                        'agent_id' => $f['id'],
                         'file_name' => $path,
+
                     ];
                 }
             } else {
                 $files[$key] = [
-                    'file_type' => $request->uploaded_file[$key]->file_type,
-                    'agent_id' => $request->uploaded_file[$key]->id,
-                    'file_name' => $request->uploaded_file[$key]->contents,
+                    'file_type' => $request->uploaded_file[$key]['file_type'],
+                    'agent_id' => $request->uploaded_file[$key]['id'],
+                    'file_name' => $request->uploaded_file[$key]['contents'],
                 ];
             }
+
             $agent_docs = AgentDocs::create($files[$key]);
             $key++;
         }
