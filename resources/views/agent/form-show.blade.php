16fa93b0d526b6be6e7d592cd47027dfa5b4d8d4
Manmeet <manmeet.k@codebrewinnovations.com>
2021-10-05 17:19:34 +0530

    fix

--- resources/views/agent/form-show.blade.php
+++ resources/views/agent/form-show.blade.php
@@ -180,6 +180,7 @@ $imgproxyurl = 'https://imgproxy.royodispatch.com/insecure/fill/90/90/sm/0/plain
             <div class="file file--upload">
                 <label for="">
                     <span class="update_pic pdf-icon">
+                        <a href="{{Storage::disk('s3')->url($agent_doc->file_name)}}" target="_blank"><img src="{{URL::asset('/assets/images/pdf.png')}}"></a>
                     </span>
                 </label>
                 <div class="invalid-feedback" id=""><strong></strong></div>
