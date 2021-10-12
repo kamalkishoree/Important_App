1e23a3979da502d959f1ac6c16f0e325bf609073
mohitv_codebrew <mohit.v@codebrewinnovations.com>
2021-10-12 18:19:34 +0530

    tst

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -371,7 +371,7 @@ class TaskController extends BaseController
                     $twilio  = new TwilioClient($twilio_sid, $token);
                     $message = $twilio->messages
                                 ->create(
-                                    '7988038082',//$customerPhoneNumber,  //to number
+                                    '+917988038082',//$customerPhoneNumber,  //to number
                                     [
                                         "body" => $sms_body,
                                         "from" => $smsProviderNumber   //form_number
@@ -398,7 +398,7 @@ class TaskController extends BaseController
                     $twilio  = new TwilioClient($twilio_sid, $token);
                     $message = $twilio->messages
                                 ->create(
-                                    '7988038082',//$recipient_phone,  //to number
+                                    '+917988038082',//$recipient_phone,  //to number
                                     [
                                         "body" => $sms_body,
                                         "from" => $smsProviderNumber   //form_number
