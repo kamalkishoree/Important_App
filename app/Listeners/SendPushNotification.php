66319012f1d929c5e055b0f1b06e1e0ec0322280
mohitv_codebrew <mohit.v@codebrewinnovations.com>
2021-10-19 16:01:03 +0530

    tst notification

--- app/Listeners/SendPushNotification.php
+++ app/Listeners/SendPushNotification.php
@@ -83,10 +83,8 @@ class SendPushNotification
      ///   Log::info($newget);
         DB::connection($schemaName)->table('rosters')->where('status',10)->delete();
         if(count($get) > 0){
-            Log::info('Roaster ssind');
-            DB::connection($schemaName)->table('rosters')->whereIn('id',$newget)->update(['status'=>1]);
-            Log::info('Roaster ssind');
-            // DB::connection($schemaName)->table('rosters')->whereIn('id',$newget)->delete();
+            // DB::connection($schemaName)->table('rosters')->whereIn('id',$newget)->update(['status'=>1]);
+            DB::connection($schemaName)->table('rosters')->whereIn('id',$newget)->delete();
             
             $this->sendnotification($get);
         }else{
