418e784c6c010eab75315537c35d68ce790771cf
root <root@ip-172-31-17-54.us-west-2.compute.internal>
2021-10-19 09:45:49 +0000

    merge

--- app/Listeners/SendPushNotification.php
+++ app/Listeners/SendPushNotification.php
@@ -84,8 +84,8 @@ class SendPushNotification
         DB::connection($schemaName)->table('rosters')->where('status',10)->delete();
         if(count($get) > 0){
             
-            // DB::connection($schemaName)->table('rosters')->whereIn('id',$newget)->update(['status'=>1]);
-            DB::connection($schemaName)->table('rosters')->whereIn('id',$newget)->delete();
+             DB::connection($schemaName)->table('rosters')->whereIn('id',$newget)->update(['status'=>1]);
+            //DB::connection($schemaName)->table('rosters')->whereIn('id',$newget)->delete();
             
             $this->sendnotification($get);
         }else{
