f17cefd91e59f67d850855cfc5629d8cd3fbd547
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-12-16 19:53:10 +0530

    seeders added to database onboarding seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -26,7 +26,8 @@ class DatabaseSeeder extends Seeder
               CmsTableSeeder::class,
               PermissionSeeder::class,
               TimezoneSeeder::class,
-              
+              PaymentOptionSeeder::class,
+              PayoutOptionSeeder::class
              ]);
     }
 }
