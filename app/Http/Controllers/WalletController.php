16b6fb6964bceb1ceaa0acf32f62618d1445d5fb
harbans <harbans.singh@codebrewinnovations.com>
2022-04-19 18:20:34 +0530

    update vnpay

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -6,7 +6,7 @@ use Session;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponser;
-use App\Model\{Agent, Transaction, PaymentOption};
+use App\Model\{Agent, Transaction, PaymentOption,User};
 use App\Http\Controllers\Controller;
 
 class WalletController extends Controller{
