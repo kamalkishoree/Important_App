ce23534b008b9d76f3eb81ffe2b2cad852e5fa6b
harbans <harbans.singh@codebrewinnovations.com>
2022-04-19 17:26:50 +0530

    update vnpay

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         "vnpay_respont",
         "payment/vnpay/notify",
-        "payment/vnpay/api"
+        "payment/vnpay/api",
+        "payment/gateway/returnResponse"
     ];
 }
