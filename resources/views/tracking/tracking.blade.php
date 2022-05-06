a925b6e796a59ab4e6241c1b30e7174969bb823a
itsowaisnoor <itsowaisnoor@gmail.com>
2022-05-06 18:15:23 +0530

    Wait Me

--- resources/views/tracking/tracking.blade.php
+++ resources/views/tracking/tracking.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name=”format-detection” content=”telephone=no”>
-    <link rel="shortcut icon" type="image/x-icon" href="{{$favicon ?? asset('assets/images/favicon.ico') }}">
+    {{-- <link rel="shortcut icon" type="image/x-icon" href="{{$favicon ?? asset('assets/images/favicon.ico') }}"> --}}
     <!-- Bootstrap CSS -->
     <!-- <link rel="stylesheet" href="css/fontawesome.css"> -->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
