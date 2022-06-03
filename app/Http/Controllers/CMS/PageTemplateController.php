b66a12b8feebcc490a8809895c54a2770b27a85e
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-06-03 17:27:34 +0530

    bugs fixed

--- app/Http/Controllers/CMS/PageTemplateController.php
+++ app/Http/Controllers/CMS/PageTemplateController.php
@@ -8,18 +8,18 @@ use Illuminate\Http\Request;
 use App\Traits\ApiResponser;
 use App\Http\Controllers\BaseController;
 use Illuminate\Support\Facades\Validator;
-use App\Model\{CMS};
+use App\Model\{Cms};
 
 class PageTemplateController extends BaseController
 {
     use ApiResponser;
 
     public function index(){
-        $templates = CMS::all();
+        $templates = Cms::all();
         return view('auth.cms.pages.index', compact('templates'));
     }
     public function show(Request $request, $domain = '', $id){
-        $templates =  CMS::where('id', $id)->first();
+        $templates =  Cms::where('id', $id)->first();
         return $this->success($templates);
     }
     public function update(Request $request, $id){
@@ -27,7 +27,7 @@ class PageTemplateController extends BaseController
             'content' => 'required',
         );
         $validation  = Validator::make($request->all(), $rules)->validate();
-        $template = CMS::where('id', $request->template_id)->firstOrFail();
+        $template = Cms::where('id', $request->template_id)->firstOrFail();
         $template->content = $request->content;
         $template->save();
         return $this->success($template, 'SMS Template Updated Successfully.');
