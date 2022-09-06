1d0d63052cd05f25a4563a7d97104118a7fd263d
harbans <harbans.singh@codebrewinnovations.com>
2022-09-06 15:21:20 +0530

    update slot controller

--- app/Model/AgentSlotDate.php
+++ app/Model/AgentSlotDate.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class AgentSlotDate extends Model
 {
     //
+    protected $fillable = ['agent_id','start_time','end_time','specific_date','working_today'];
+
+    public function deleteVendorSlotDates($agent_id)
+    {
+    	return $this->where('agent_id',$agent_id)->delete();
+    }
+
 }
--- app/Model/SlotDay.php
+++ app/Model/SlotDay.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class SlotDay extends Model
 {
-    //
+    protected $fillable = ['slot_id','day'];
+
+    public function agent_slot(){
+	    return $this->belongsTo('App\Model\AgentSlot','slot_id','id'); 
+	}
 }
