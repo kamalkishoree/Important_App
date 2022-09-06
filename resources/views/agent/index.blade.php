1a06a200c79c5ecc0e00f8cb23c500a0d12aaf28
surendradbit <surendrasingh@codebrewinnovations.com>
2022-09-06 16:05:15 +0530

    dispatcher layout

--- resources/views/agent/index.blade.php
+++ resources/views/agent/index.blade.php
@@ -178,7 +178,27 @@ $imgproxyurl = 'https://imgproxy.royodispatch.com/insecure/fill/90/90/sm/0/plain
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <ul class="nav nav-tabs nav-material" id="top-tab" role="tablist">
+                    <div class="alFilterLocation">
+                        <div class="row d-flex justify-content-end">
+                            <div class="col-sm-2">
+                                <select name="geo_filter" id="geo_filter" class="form-control">
+                                    <option value="">{{__('Filter by location')}}</option>
+                                    @foreach($geos as $geo)
+                                        <option value="{{$geo->id}}">{{$geo->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-sm-2">
+                                <select name="tag_filter" id="tag_filter" class="form-control">
+                                    <option value="">{{__('Filter by tags')}}</option>
+                                    @foreach($tags as $tag)
+                                        <option value="{{$tag->id}}">{{$tag->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="nav nav-tabs nav-material alNavTopMinus" id="top-tab" role="tablist">
                         <li class="nav-item">
                             <a class="nav-link active" id="active-vendor" data-toggle="tab" href="#active_vendor" role="tab" aria-selected="false" data-rel="agent-listing" data-status="1">
                                 <i class="icofont icofont-man-in-glasses"></i>{{ __('Active') }}<sup class="total-items" id="active_vendor_count">({{$agentIsApproved}})</sup>
@@ -198,29 +218,10 @@ $imgproxyurl = 'https://imgproxy.royodispatch.com/insecure/fill/90/90/sm/0/plain
                             <div class="material-border"></div>
                         </li>
                     </ul>
-                    <div class="row mt-3">
-                        <div class="col-sm-2">
-                            <label for="geo_filter">{{__('Filter by location')}}</label>
-                            <select name="geo_filter" id="geo_filter" class="form-control">
-                                <option value="">{{__('All')}}</option>
-                                @foreach($geos as $geo)
-                                    <option value="{{$geo->id}}">{{$geo->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-sm-2">
-                            <label for="tag_filter">{{__('Filter by tags')}}</label>
-                            <select name="tag_filter" id="tag_filter" class="form-control">
-                                <option value="">{{__('All')}}</option>
-                                @foreach($tags as $tag)
-                                    <option value="{{$tag->id}}">{{$tag->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
+                    
                     <div class="tab-content nav-material pt-0" id="top-tabContent">
                         <div class="tab-pane fade past-order show active" id="active_vendor" role="tabpanel" aria-labelledby="active-vendor">
-                            <div class="row mb-2 mt-3">
+                            <div class="row mb-2 mt-2">
                                 <div class="col-sm-12">
                                     <div class="text-sm-left">
                                         @if (\Session::has('success'))
@@ -235,19 +236,16 @@ $imgproxyurl = 'https://imgproxy.royodispatch.com/insecure/fill/90/90/sm/0/plain
                                     <button type="button" class="btn btn-success waves-effect waves-light saveaccounting" data-toggle="modal" data-target="#pay-receive-modal" data-backdrop="static" data-keyboard="false">{{__("Pay")}} / {{__("Receive")}}</button>
                                 </div>
                             </div>
-
                             <div class="table-responsive nagtive-margin">
                                 <table class="table table-striped dt-responsive nowrap w-100 all agent-listing" id="agent-listing">
                                     <thead>
                                         <tr>
                                             <th class="sort-icon">{{__("Agent ID")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
-                                            {{-- <th class="sort-icon">{{__("UID")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th> --}}
                                             <th class="sort-icon">{{__("Profile")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
                                             <th class="sort-icon">{{__("Name")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
                                             <th class="sort-icon">{{__("Phone")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
                                             <th class="sort-icon">{{__("Type")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
                                             <th class="sort-icon">{{__("Team")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
-                                            {{-- <th class="sort-icon">{{__("Vehicle")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th> --}}
                                             <th class="sort-icon">{{__("Cash Collected")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
                                             <th class="sort-icon">{{__("Order Earning")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
                                             <th class="sort-icon">{{__("Total Paid to Agent")}} <i class="fa fa-sort ml-1" aria-hidden="true"></i></th>
