6392bc366b913c9c1b84f1673f652dacea647748
harbans <harbans.singh@codebrewinnovations.com>
2022-09-23 12:22:17 +0530

    slot edder

--- app/Http/Controllers/Api/AgentSlotController.php
+++ app/Http/Controllers/Api/AgentSlotController.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\Api\AgentController;
 
 class AgentSlotController extends BaseController
 {
-    protected function successResponse($data, $message = null, $code = 200)
+    public function successResponse($data, $message = null, $code = 200)
 	{
 		return response()->json([
 			'status' => 'Success',
@@ -27,7 +27,7 @@ class AgentSlotController extends BaseController
 			'data' => $data
 		], $code);
 	}
-    protected function errorResponse($message = null, $code, $data = null)
+    public function errorResponse($message = null, $code, $data = null)
 	{
 		return response()->json([
 			'status' => 'Error',
