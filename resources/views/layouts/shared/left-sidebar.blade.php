211f35bd9491d2281f6a126054c1c192d6ac885d
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2022-11-03 13:04:06 +0530

    changes

--- resources/views/layouts/shared/left-sidebar.blade.php
+++ resources/views/layouts/shared/left-sidebar.blade.php
@@ -262,17 +262,6 @@
                 </li>
                 @endif
 
-
-                @if(in_array('ACL',$allowed) || Auth::user()->is_superadmin == 1)
-                <li>
-                    <a href="{{route('subadmins.index')}}">
-                        <i class="fe-users"></i>
-                        <span> {{__('Managers')}} </span>
-                    </a>
-                </li>
-                
-                @endif
-                </li>
                 <li>
                     <a href="{{route('driver-accountancy.index')}}">
                         <i class="fe-users"></i>
