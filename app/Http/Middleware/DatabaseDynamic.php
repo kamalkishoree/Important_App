df05e3a0b3b93761cbd161c0d4cc89528e2b2d7a
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-11-03 11:07:44 +0530

    done razorpay payout mobile driver

--- app/Http/Middleware/DatabaseDynamic.php
+++ app/Http/Middleware/DatabaseDynamic.php
@@ -22,7 +22,6 @@ class DatabaseDynamic
      */
     public function handle($request, Closure $next)
     {
-        dd('dssd');
         if(Auth::check()){
          
           $client = Auth::user();
