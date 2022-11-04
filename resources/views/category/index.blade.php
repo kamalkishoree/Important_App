b364c8280fdc2c4febb90fd0eb4815676c46c444
Praveen121 <praveen.kumar@codebrewinnovations.com>
2022-11-04 11:53:45 +0530

    light theme bug fixing

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -26,7 +26,7 @@
                     <div class="card-body">
                         <div class="row mb-2">
                             <div class="col-sm-7"></div>
-                            <div class="col-sm-5 text-right btn-auto d-flex" style="left: 115px;">
+                            <div class="col-sm-5 text-right btn-auto d-flex justify-content-end">
                                 <button type="button" class="btn btn-blue waves-effect waves-light openAddProductModal"><i class="mdi mdi-plus-circle mr-1"></i> {{__("Add Product")}}</button>
 
                                 <button type="button" class="btn btn-blue waves-effect waves-light openCategoryModal" data-toggle="modal" data-target="" data-backdrop="static" data-keyboard="false"><i class="mdi mdi-plus-circle mr-1"></i> {{__("Add Category")}}</button>
