a2eecaa077119178faba1def65a473982cddf822
sandeep kumar <sandeep.kumar@codebrewinnovations.com>
2023-03-14 19:03:32 +0530

    Relate agent filter fixes

--- app/Http/Controllers/Accountancy/DriverAccountingController.php
+++ app/Http/Controllers/Accountancy/DriverAccountingController.php
@@ -56,13 +56,12 @@ class DriverAccountingController extends BaseController
         $user = Auth::user();
         $userid = $user->id;
         $type = $request->routesListingType;
+        $orders = Order::with(['agent', 'getAgentPayout']);
         if ( $user->is_superadmin == 0 &&  $user->all_team_access == 0) {
-            $orders = Order::with(['agent', 'getAgentPayout'])->whereHas('agent.team.permissionToManager', function($q) use ($userid){
+            $orders = $orders->whereHas('agent.team.permissionToManager', function($q) use ($userid){
             $q->where('sub_admin_id', $userid);
-        });
-        }else{
-            $orders = Order::with(['agent', 'getAgentPayout']);
-        }
+        });}
+        
         if($type == 'statement') {
             $orders = $orders->whereHas('getAgentPayout' , function($query) use($type) {
                 $query->where('status', 1);
