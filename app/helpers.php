6967546e6d1af0f47bc18aff791b35c963bbe045
shabnamnazir <shabnam@codebrewinnovations.com>
2023-04-06 17:47:57 +0530

    driver push notification

--- app/helpers.php
+++ app/helpers.php
@@ -393,33 +393,3 @@ if (!function_exists('decimal_format')) {
             return $km;
         }
     }
-
-    function sendFcmCurlRequest($data ,$fcm_server_key = '')
-    {
-   
-        $fcm_server_key = ($fcm_server_key =='') ? ClientPreference::select('fcm_server_key')->first()->fcm_server_key :  $fcm_server_key ;
-
-         if (!empty($fcm_server_key )) {
-           
-            $headers = [
-                'Authorization: key='.$fcm_server_key ,
-                'Content-Type: application/json',
-            ];
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-            $result = curl_exec($ch);
-            // if ($result === FALSE) {
-            //     die('Oops! FCM Send Error: ' . curl_error($ch));
-            // }
-            curl_close($ch);
-       
-            return $result;
-        } else {
-            return false;
-        }
-    }
