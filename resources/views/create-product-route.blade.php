cbc2ef50490004938f065a0472910ada73410367
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-05-04 13:14:40 +0530

    resolved discussion

--- resources/views/create-product-route.blade.php
+++ resources/views/create-product-route.blade.php
@@ -37,7 +37,7 @@
 									<div class="form-check">
 										<input type="checkbox" class="form-check-input"
 											onchange="getCategory({{$cat->id}})" name="category-check"
-											id="category-check_{{$cat->id}}"> <label class="label-check" for="category-check_{{$cat->id}}">{{ $cat->translation->name}}</label>
+											id="category-check_{{$cat->id}}"> <label class="label-check" for="category-check_{{$cat->id}}">{{ !empty($cat->translation) ? $cat->translation->name:$cat->slug}}</label>
 									</div>
 								</li> @endforeach
 
