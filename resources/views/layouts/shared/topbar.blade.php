0bab4312130cb409481867e374deab8914e530dd
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-05-09 17:24:14 +0530

    dispatcher add route changes

--- resources/views/layouts/shared/topbar.blade.php
+++ resources/views/layouts/shared/topbar.blade.php
@@ -128,7 +128,7 @@ $clientData = \App\Model\Client::select('id', 'logo','custom_domain','code')->wi
           
             <li class="d-lg-inline-block" >
                 <a class="nav-link" href="#">   <!-- addTaskModalHeader -->
-                    <button type="button" class="btn btn-blue waves-effect waves-light  klklkl" data-toggle="modal" data-target="#addRouteModal" data-backdrop="static" title="{{__('Add Route')}}" data-keyboard="false"><span><i class="mdi mdi-plus-circle mr-1"></i> {{__('Add Route')}}</span></button>
+                    <button type="button" class="btn btn-blue waves-effect waves-light  klklkl" data-toggle="modal" data-target="#addRouteModal" id="route-btn" data-backdrop="static" title="{{__('Add Route')}}" data-keyboard="false"><span><i class="mdi mdi-plus-circle mr-1"></i> {{__('Add Route')}}</span></button>
                 </a>
             </li>
            @endif
