2d4af9670c663f9841124de105a98313ce32c8f8
Yogesh Rana <yogesh.rana@codebrewinnovations.com>
2023-06-05 13:05:17 +0530

    comment

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -4001,7 +4001,7 @@ class TaskController extends BaseController
             Order::where('id', $orders->id)->update($updateorder);
 
             if (isset($request->customer_phone_number)) {
-                $this->sendSms2($request->customer_phone_number, $dispatch_traking_url);
+                // $this->sendSms2($request->customer_phone_number, $dispatch_traking_url);
             }
 
             DB::commit();
