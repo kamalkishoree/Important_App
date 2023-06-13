ec9bd6d4f47643efc2d53751168de29081bd0125
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-06-13 18:50:24 +0530

    timeissue

--- app/Jobs/RosterDelete.php
+++ app/Jobs/RosterDelete.php
@@ -56,13 +56,13 @@ class RosterDelete implements ShouldQueue
             Config::set("database.connections.$schemaName", $default);
             config(["database.connections.mysql.database" => $schemaName]);
             if($this->type=='B'){
-                DB::connection($schemaName)->table('rosters')->where('batch_no',$this->order_id)->where('is_particular_driver','!=',2)->delete();
+                DB::connection($schemaName)->table('rosters')->where('batch_no',$this->order_id)->where('is_particular_driver','=',0)->delete();
             }else if($this->type=='PD'){
                 DB::connection($schemaName)->table('rosters')->where('order_id',$this->order_id)
                 ->where('is_particular_driver',0)
                 ->delete();
             }else{
-                DB::connection($schemaName)->table('rosters')->where('order_id',$this->order_id)->where('is_particular_driver','!=',2)->delete();
+                DB::connection($schemaName)->table('rosters')->where('order_id',$this->order_id)->where('is_particular_driver',0)->delete();
             }
             DB::disconnect($schemaName);
         } catch (Exception $ex) {
