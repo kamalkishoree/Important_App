4820b68ba195188fd7c9d5fc2a1af9cc14015abf
inderjit <inderjit.singh@codebrewinnovation.com>
2023-06-15 16:58:19 +0530

    done remove handle from schedual notification

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -1262,7 +1262,7 @@ class TaskController extends BaseController
                 $settime = $request->schedule_time;
                 //Check Api call from Mobile side = 1 or website = 0
                 if($app_call){
-                    // date_default_timezone_set($clienttimezone);
+                    date_default_timezone_set($clienttimezone);
                     $settime = Carbon::createFromFormat('Y-m-d H:i:s', $request->schedule_time.':00')->setTimezone('UTC');
                 }
 
