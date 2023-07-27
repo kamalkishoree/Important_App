4107cb47e7a0ac2b90ee347b3c00355ec311d73c
inderwadhs <inderwadhs@gmail.com>
2023-07-27 19:29:24 +0530

    done add changes

--- app/Listeners/SendPushNotification.php
+++ app/Listeners/SendPushNotification.php
@@ -76,8 +76,10 @@ class SendPushNotification
                                     ->leftJoin('roster_details', 'rosters.detail_id', '=', 'roster_details.unique_id')
                                     ->select('rosters.*', 'roster_details.customer_name', 'roster_details.customer_phone_number',
         'roster_details.short_name','roster_details.address','roster_details.lat','roster_details.long','roster_details.task_count');
-        $getids           = $get->pluck('id');
+        $getids           = $get->pluck('id')->toArray();
         $get              = $get->get();
+        \Log::info([$getids]);
+        \Log::info([$get]);
         DB::connection($schemaName)->table('rosters')->where('status',10)->delete();
         if(count($get) > 0){
             DB::connection($schemaName)->table('rosters')->whereIn('id',$getids)->update(['status'=>1]);
