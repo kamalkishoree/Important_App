c5bdeaf763a0f17b15baf6aed743fac12b7e01f1
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-07-28 20:12:50 +0530

    sound change in notification pre_stage

--- app/Jobs/RosterDelete.php
+++ app/Jobs/RosterDelete.php
@@ -160,7 +160,7 @@ class RosterDelete implements ShouldQueue
                                 ->notification([
                                     'title'              => 'Pickup Request',
                                     'body'               => 'Check All Details For This Request In App',
-                                    'sound'              => 'notification',
+                                    'sound'              => 'notification.mp3',
                                     'android_channel_id' => 'Royo-Delivery',
                                     'soundPlay'          => true,
                                     'show_in_foreground' => true,
